6c58a11f43f0cf71161a557b3b6e313518857ceb
Alberto Hernandez Alvarez <albertoha1990@outlook.es>
2021-02-15 14:55:11 +0100

    - Añadidas funcionalidades de resta y multiplicación

--- src/com/codigonline/Calculadora.java
+++ src/com/codigonline/Calculadora.java
@@ -5,4 +5,12 @@ public class Calculadora {
     public int suma(int num1, int num2){
         return num1+num2;
     }
+
+    public int restar(int num1, int num2){
+        return num1-num2;
+    }
+
+    public int mult(int num1, int num2){
+        return num1 * num2;
+    }
 }
